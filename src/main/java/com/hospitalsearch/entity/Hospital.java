4c1224c09e0e95af794319c7c834b752c8bb90b4
Oleksandr Mukonin <alex.mukonin@gmail.com>
2016-05-19 00:21:04 +0300

    added hibernate annotations for min/max

--- src/main/java/com/hospitalsearch/entity/Hospital.java
+++ src/main/java/com/hospitalsearch/entity/Hospital.java
@@ -1,14 +1,15 @@
 package com.hospitalsearch.entity;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.NamedQueries;
 import org.hibernate.annotations.NamedQuery;
@@ -24,46 +25,54 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @NamedQueries
 (
-	{
-		@NamedQuery(name = Hospital.DELETE_HOSPITAL_BY_ID, query = Hospital.DELETE_HOSPITAL_BY_ID_QUERY),
-		@NamedQuery(name = Hospital.GET_LIST_BY_BOUNDS, query = Hospital.GET_LIST_BY_BOUNDS_QUERY)
-	}
-)
+		{
+			@NamedQuery(name = Hospital.DELETE_HOSPITAL_BY_ID, query = Hospital.DELETE_HOSPITAL_BY_ID_QUERY),
+			@NamedQuery(name = Hospital.GET_LIST_BY_BOUNDS, query = Hospital.GET_LIST_BY_BOUNDS_QUERY)
+		}
+		)
 public class Hospital{
-	
+
 	static final String GET_LIST_BY_BOUNDS_QUERY = "from Hospital h where "
 			+ "(latitude < :nelat) and "
 			+ "(latitude > :swlat) and "
 			+ "(longitude < :nelng) and "
 			+ "(longitude > :swlng)";
 	public static final String GET_LIST_BY_BOUNDS = "GET_LIST_BY_BOUNDS";
-	
+
 	static final String DELETE_HOSPITAL_BY_ID_QUERY = "DELETE Hospital WHERE id = :id";
 	public static final String DELETE_HOSPITAL_BY_ID = "DELETE_HOSPITAL_BY_ID";
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@NotEmpty
+	@Size(min = 10, max = 50)
 	@Column(name = "NAME", nullable = false)
 	private String name;
 
 	@NotNull
+	@Min(-90)
+	@Max(90)
 	@Column(name = "LATITUDE", nullable = false)
 	private Double latitude;
 
 	@NotNull
+	@Min(-180)
+	@Max(180)
 	@Column(name = "LONGITUDE", nullable = false)
 	private Double longitude;
-	
+
 	@NotEmpty
+	@Size(min = 10, max = 100)
 	@Column(name = "ADDRESS", nullable = false)
 	private String address; 
-	
+
+
+	@Size(max = 150)
 	@Column(name = "DESCRIPTION", nullable = false)
 	private String description; 
-	
+
 	private String imagePath;
 
 	public Long getId() {
