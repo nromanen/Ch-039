b341ed64e0e42992d587fd1d150fd7e8f0a18b88
ilay888@yandex.ua <lich1993>
2016-06-01 16:59:38 +0300

    illia new 2 changes

--- src/main/java/com/hospitalsearch/controller/FeedbackController.java
+++ src/main/java/com/hospitalsearch/controller/FeedbackController.java
@@ -37,7 +37,6 @@ public class FeedbackController {
         
     	User producer = feedbackService.getByUserEmail(dto.getUserEmail());
     	User consumer = userService.getById(dto.getDoctorId());
-    		
     	feedbackService.save(dto.buildFeedback(consumer, producer));
     	return "true";
     }
