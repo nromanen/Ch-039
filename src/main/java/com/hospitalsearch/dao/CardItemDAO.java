62b609c50f4adbee76e37322bcd7b038b45ad66a
Oleksandr Mukonin <alex.mukonin@gmail.com>
2016-06-01 16:07:12 +0300

    remove .idea

--- src/main/java/com/hospitalsearch/dao/CardItemDAO.java
+++
@@ -1,9 +0,0 @@
-package com.hospitalsearch.dao;
-
-import com.hospitalsearch.dao.GenericDAO;
-import com.hospitalsearch.entity.CardItem;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface CardItemDAO extends GenericDAO<CardItem, Long> {
-}
